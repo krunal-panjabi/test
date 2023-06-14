a28d7857bb6a5dd51a3b5db6378129c8595b6ca3
krunal-panjabi <krunalpanjabi2002@gmail.com>
2023-06-14 18:31:10 +0530

    Add project files.

---
+++ trykrunal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace trykrunal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
